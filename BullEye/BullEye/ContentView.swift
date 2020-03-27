ba2cc4f94849a89b62db03b4f52ff9a4541d92e8
Joash Tubaga <joashtubaga@gmail.com>
2020-03-28 00:20:15 +0800

    [✨] Added action button and a pop up diaglog box.

--- BullEye/BullEye/ContentView.swift
+++ BullEye/BullEye/ContentView.swift
@@ -9,20 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(.green)
-            Button(action: {}) {
-                Text("Hit Me!")
-            }
-        }
-    }
+	
+	@State var alertIsVisible: Bool = false
+	
+	var body: some View {
+		VStack {
+			Text("Welcome to my first app!")
+				.fontWeight(.semibold)
+				.foregroundColor(.green)
+			Button(action: {
+				print("Button pressed!")
+				self.alertIsVisible = true
+			}) {
+				Text("Hit Me!")
+			}
+			.alert(isPresented: $alertIsVisible) { () -> Alert in
+				return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up!"), dismissButton: .default(Text("Awesome!")))
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
